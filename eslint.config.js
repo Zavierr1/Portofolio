f78c2862584a5ea8c2885094b7fde73333842bf6
Fier <muhammad.jafier@student.president.ac.id>
2025-08-08 06:21:43 +0700

    improve the projects config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,6 +17,7 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
